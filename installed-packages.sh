44f05fc6cb7c2425fff304b79cbcc46da7ba0ae5
Adam Nord <adam.nord@abbgymnasiet.se>
2022-11-07 18:43:57 +0100

    fixed install-packages script

--- installed-packages.sh
+++ installed-packages.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 echo "Pacman packages:" > installed-packages
-pacman -Qe > pacman-packages >> installed-packages
+pacman -Qe >> installed-packages
 echo "AUR packages:" >> installed-packages
-pacman -Qm > aur-packages >> installed-packages
-echo "Global NPM packages:" >> installed-packages
-npm -g ls > npm-packages >> installed-packages
+pacman -Qm >> installed-packages
