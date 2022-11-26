128a7bdf7ec7ff681ab5bdc6242f657b930e81a3
Adam Nord <adam.nord@abbgymnasiet.se>
2022-11-26 17:25:06 +0100

    revert merge
    This reverts commit 314d5b7fb8822b431ac50da5c2a18481c162676c, reversing
    changes made to 2f45d3b179e002ba9b7afc110ca3d3962f5a6b7b.

--- hyprland/.config/hypr/scripts/center-and-float.sh
+++
@@ -1,5 +0,0 @@
-is_floating=$(hyprctl activewindow -j | jq '.floating')
-if [ $is_floating = 'false' ]
-then
-    hyprctl --batch "dispatch togglefloating active;dispatch resizeactive exact 1000 700; dispatch centerwindow active"
-fi
